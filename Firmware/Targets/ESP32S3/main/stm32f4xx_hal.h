047169a19190c8e319bda08fc53be7ec50920677
OrhanYigitDurmaz <orhanyigitv2durmaz@gmail.com>
2025-05-09 23:46:52 +0300

    update stm32f4xx hal to include spi, also add new raw define file

--- Firmware/Targets/ESP32S3/main/stm32f4xx_hal.h
+++ Firmware/Targets/ESP32S3/main/stm32f4xx_hal.h
@@ -8,8 +8,8 @@
  * so im doing it like this now...
  *
  * Also: this file has most of our defines:
- * https://github.com/Ultrawipf/OpenFFBoard/blob/32fcf36c9f73127fb788d3d61381a773c9a5bb04/Firmware/Targets/F407VG/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h
- * This file is os big, it doesnt get indexed by github and as a result you dont find what you need.
+ * https://raw.githubusercontent.com/Ultrawipf/OpenFFBoard/refs/heads/development/Firmware/Targets/F407VG/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h
+ * This file is pretty big, it doesnt get indexed by github and as a result you dont find what you need.
  * If you cant find something look here first.
  */
 
@@ -248,3 +248,95 @@ typedef struct
 
 
 //--------------------- stm32f4xx_hal_spi ---------------------//
+
+typedef struct
+{
+  __IO uint32_t CR1;
+  __IO uint32_t CR2;
+  __IO uint32_t SR;
+  __IO uint32_t DR;
+  __IO uint32_t CRCPR;
+  __IO uint32_t RXCRCR;
+  __IO uint32_t TXCRCR;
+  __IO uint32_t I2SCFGR;
+  __IO uint32_t I2SPR;
+} SPI_TypeDef;
+
+typedef struct
+{
+  uint32_t Mode;
+  uint32_t Direction;
+  uint32_t DataSize;
+  uint32_t CLKPolarity;
+  uint32_t CLKPhase;
+  uint32_t NSS;
+  uint32_t BaudRatePrescaler;
+  uint32_t FirstBit;
+  uint32_t TIMode;
+  uint32_t CRCCalculation;
+  uint32_t CRCPolynomial;
+} SPI_InitTypeDef;
+
+typedef enum
+{
+  HAL_SPI_STATE_RESET      = 0x00U,    /*!< Peripheral not Initialized                         */
+  HAL_SPI_STATE_READY      = 0x01U,    /*!< Peripheral Initialized and ready for use           */
+  HAL_SPI_STATE_BUSY       = 0x02U,    /*!< an internal process is ongoing                     */
+  HAL_SPI_STATE_BUSY_TX    = 0x03U,    /*!< Data Transmission process is ongoing               */
+  HAL_SPI_STATE_BUSY_RX    = 0x04U,    /*!< Data Reception process is ongoing                  */
+  HAL_SPI_STATE_BUSY_TX_RX = 0x05U,    /*!< Data Transmission and Reception process is ongoing */
+  HAL_SPI_STATE_ERROR      = 0x06U,    /*!< SPI error state                                    */
+  HAL_SPI_STATE_ABORT      = 0x07U     /*!< SPI abort is ongoing                               */
+} HAL_SPI_StateTypeDef;
+
+#define SPI_POLARITY_LOW                (0x00000000U)
+#define SPI_POLARITY_HIGH               (0x00000001U)
+
+#define SPI_MODE_SLAVE                  (0x00000000U)
+#define SPI_MODE_MASTER                 (0x00000001U)
+
+#define SPI_DIRECTION_2LINES            (0x00000000U)
+#define SPI_DIRECTION_2LINES_RXONLY     (0x00000001U)
+#define SPI_DIRECTION_1LINE             (0x00000002U)
+
+#define SPI_DATASIZE_8BIT               (0x00000000U)
+#define SPI_DATASIZE_16BIT              (0x00000001U)
+
+#define SPI_PHASE_1EDGE                 (0x00000000U)
+#define SPI_PHASE_2EDGE                 (0x00000001U)
+
+#define SPI_NSS_SOFT                    (0x00000001U)
+#define SPI_NSS_HARD_INPUT              (0x00000000U)
+#define SPI_NSS_HARD_OUTPUT             (0x00000002U)
+
+#define SPI_BAUDRATEPRESCALER_2         (0x00000000U)
+#define SPI_BAUDRATEPRESCALER_4         (0x00000001U)
+#define SPI_BAUDRATEPRESCALER_8         (0x00000002U)
+#define SPI_BAUDRATEPRESCALER_16        (0x00000003U)
+#define SPI_BAUDRATEPRESCALER_32        (0x00000004U)
+#define SPI_BAUDRATEPRESCALER_64        (0x00000005U)
+#define SPI_BAUDRATEPRESCALER_128       (0x00000006U)
+#define SPI_BAUDRATEPRESCALER_256       (0x00000007U)
+
+#define SPI_FIRSTBIT_MSB                (0x00000000U)
+#define SPI_FIRSTBIT_LSB                (0x00000001U)
+
+typedef struct
+{
+  SPI_TypeDef                *Instance;
+  SPI_InitTypeDef            Init;
+  const uint8_t              *pTxBuffPtr;
+  uint16_t                   TxXferSize;
+  __IO uint16_t              TxXferCount;
+  uint8_t                    *pRxBuffPtr;
+  uint16_t                   RxXferSize;
+  __IO uint16_t              RxXferCount;
+  //void (*RxISR)(struct __SPI_HandleTypeDef *h
+  //void (*TxISR)(struct __SPI_HandleTypeDef *h
+  //DMA_HandleTypeDef          *hdmatx;
+  //DMA_HandleTypeDef          *hdmarx;
+  HAL_LockTypeDef            Lock;
+  __IO HAL_SPI_StateTypeDef  State;
+  __IO uint32_t              ErrorCode;
+
+} SPI_HandleTypeDef;
